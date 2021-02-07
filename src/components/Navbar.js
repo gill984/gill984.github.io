e997ae6fd11fdc28275262aac8525e8cf70482f6
Michael Gill <michaeljgill@ufl.edu>
2021-02-07 02:52:52 -0500

    Updated some more wording

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
             Portfolio.
           </div>
           <div className="links-wrapper">
-            <button onClick={() => scrollTo("#work")}>Work</button>
+            <button onClick={() => scrollTo("#work")}>Projects</button>
             <button onClick={() => scrollTo("#about")}>About</button>
             <button onClick={() => scrollTo("#contact")}>Contact</button>
           </div>
